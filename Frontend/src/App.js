c964d41093152aa24b38ba600be83049c3350a73
Jules! <69649546+Dayucoffee@users.noreply.github.com>
2023-06-16 16:19:41 +0100

    Add files via upload

---
+++ Frontend/src/App.js
@@ -0,0 +1,15 @@
+import AddMovie from './AddMovie'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+          <AddMovie/>
+            
+          </div>
+        );
+      }
+  
+export default App;
